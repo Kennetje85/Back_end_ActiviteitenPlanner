a9f42e65e8f94e5dc1368dea10ccf148eae0405a
Kwok,Ken K.C <k.kwok@student.fontys.nl>
2026-05-31 15:50:03 +0200

    add connectionstring

--- AppDbContextFactory.cs
+++ AppDbContextFactory.cs
@@ -1,5 +1,8 @@
+using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace Backend_ActiviteitenPlanner
 {
@@ -7,8 +10,21 @@ namespace Backend_ActiviteitenPlanner
     {
         public AppDbContext CreateDbContext(string[] args)
         {
+            // Priority: environment variable -> appsettings.json -> Azure SQL fallback
+            var envConn = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            var configConn = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build()
+                .GetConnectionString("Default");
+
+            var connectionString = envConn
+                ?? configConn
+                ?? "Server=tcp:dbhhs.database.windows.net,1433;Initial Catalog=Activiteitenplanner;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";";
+
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer("Server=DESKTOP-6C6PF5S\\SQLEXPRESS;Database=ActivityPlanner;Trusted_Connection=True;Encrypt=False;");
+            optionsBuilder.UseSqlServer(connectionString);
             return new AppDbContext(optionsBuilder.Options);
         }
     }
--- Migrations/20260531134249_AddActivities.Designer.cs
+++ Migrations/20260531134249_AddActivities.Designer.cs
@@ -4,6 +4,7 @@ using Backend_ActiviteitenPlanner;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend_ActiviteitenPlanner.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260531134249_AddActivities")]
+    partial class AddActivities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
