e79622065225b6b47006ceb9b0445367c78c86e3
Kwok,Ken K.C <k.kwok@student.fontys.nl>
2026-06-04 09:56:48 +0200

    config connectionstring

---
+++ Controllers/RegistrationsController.cs
@@ -0,0 +1,75 @@
+﻿using Backend_ActiviteitenPlanner.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend_ActiviteitenPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RegistrationsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/registrations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Registration>>> GetRegistrations()
+        {
+            return await _context.Registrations
+                .Include(r => r.Activity)
+                .Include(r => r.User)
+                .ToListAsync();
+        }
+
+        // GET: api/registrations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Registration>> GetRegistration(int id)
+        {
+            var registration = await _context.Registrations
+                .Include(r => r.Activity)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (registration == null)
+                return NotFound();
+
+            return registration;
+        }
+
+        // POST: api/registrations
+        [HttpPost]
+        public async Task<ActionResult<Registration>> PostRegistration(Registration registration)
+        {
+            registration.CreatedAt = DateTime.UtcNow;
+
+            _context.Registrations.Add(registration);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRegistration), new { id = registration.Id }, registration);
+        }
+
+        // DELETE: api/registrations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRegistration(int id)
+        {
+            var registration = await _context.Registrations.FindAsync(id);
+
+            if (registration == null)
+                return NotFound();
+
+            _context.Registrations.Remove(registration);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
